74ff238c64d9a156d529f13cda22f0ac729f919e
edimart28 <edimart28@gmail.com>
2024-08-06 12:52:23 -0500

    taller java basico dos

---
+++ src/For.java
@@ -0,0 +1,16 @@
+public class For {
+
+    public static void main(String[] args) {
+
+        int suma = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            suma += i;
+
+            System.out.println("La sumatoria de los numeros enteros entre 1 y 10 es " +suma);
+
+
+        }
+    }
+
+}
---
+++ src/While.java
@@ -0,0 +1,22 @@
+public class While {
+
+    public static void main(String[] args) {
+
+        int suma = 0;
+        int i = 1;
+
+        while (i <= 10){
+            suma += i;
+
+            i++;
+        }
+
+
+
+        System.out.println("La suma es: " + suma);
+
+
+
+    }
+
+}
